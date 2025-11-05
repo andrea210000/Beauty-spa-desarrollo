2611c211e9c3a9ace9d17eb3a1fd15c127b5c9f6
Felipexf <anonimusoriginal26@gmail.com>
2025-11-04 22:28:27 -0500

    actualizacion del proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './views/shared/menu-component/menu-component';
 import { ScheduleComponent } from './views/shared/schedule-component/schedule-component';
+import {ErrorComponent} from './views/shared/error-component/error-component';
+import {LocationComponent} from './views/shared/location-component/location-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, ScheduleComponent],
+  imports: [RouterOutlet, MenuComponent, ScheduleComponent, ErrorComponent, LocationComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
