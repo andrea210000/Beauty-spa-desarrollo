1c52fb04a257f7b065668f5610ebb11ef48ca3fc
RORRIZ19 <rodrilop320@hotmail.com>
2025-11-04 22:28:21 -0500

    actualizacion del proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './views/shared/menu-component/menu-component';
 import { ScheduleComponent } from './views/shared/schedule-component/schedule-component';
+import { FooterComponent} from './views/shared/footer-component/footer-component';
+import {AboutUsComponent } from './views/shared/about-us-component/about-us-component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, ScheduleComponent],
+  imports: [RouterOutlet, MenuComponent, ScheduleComponent, FooterComponent, AboutUsComponent ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
