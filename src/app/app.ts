c366ac8d0197e03d25859284b343ab04f84862de
RORRIZ19 <rodrilop320@hotmail.com>
2025-11-09 22:39:29 -0500

    Feature/rodrigo (#8)
    * actualizacion del proyecto
    
    * creacion de footer y about us

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,16 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './views/shared/menu-component/menu-component';
 import { ScheduleComponent } from './views/shared/schedule-component/schedule-component';
+import { FooterComponent} from './views/shared/footer-component/footer-component';
+import {AboutUsComponent } from './views/shared/about-us-component/about-us-component'
 import {ErrorComponent} from './views/shared/error-component/error-component';
 import {LocationComponent} from './views/shared/location-component/location-component';
 
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, ScheduleComponent, ErrorComponent, LocationComponent],
+  imports: [RouterOutlet, MenuComponent, ScheduleComponent, ErrorComponent, LocationComponent,FooterComponent,AboutUsComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
