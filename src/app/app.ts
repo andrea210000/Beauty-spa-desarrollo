26d1e8e7088187d3805b78dd0ed19151d7135c47
calderon00709 <jhojanalexisgonzales@gmail.com>
2025-11-09 23:07:24 -0500

    creacion de contact component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './views/shared/menu-component/menu-component';
 import { ScheduleComponent } from './views/shared/schedule-component/schedule-component';
-
+import { ContactComponent } from './views/shared/contact-component/contact-component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, ScheduleComponent],
+  imports: [RouterOutlet, MenuComponent, ScheduleComponent, ContactComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
