b174afea93a287ff98a55e645b9be141b7bff6f0
andrea210000 <andreacarolinaanaya2019@gmail.com>
2026-03-29 01:13:49 -0500

    Agregar productos al catálogo, carrito funcional con cálculos, hover en botones

--- src/app/views/our-products-component/our-products-component.ts
+++ src/app/views/our-products-component/our-products-component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-our-products-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './our-products-component.html',
   styleUrl: './our-products-component.css',
 })
-export class OurProductsComponent {
+export class OurProductsComponent implements OnInit {
+  
+  productos: any[] = [];
 
+  constructor(private productService: ProductService, private cartService: CartService) {}
+
+  ngOnInit() {
+    this.productos = this.productService.getProductos();
+  }
+  
+  getProductosPorCategoria(categoria: string) {
+    return this.productos.filter(p => p.category === categoria);
+  }
+
+  agregarAlCarrito(producto: any) {
+    this.cartService.addProduct(producto);
+  }
 }
