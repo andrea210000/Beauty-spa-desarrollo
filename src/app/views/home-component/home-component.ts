05fe108427e4dc6f99ee81f917e3c27690fab335
andrea210000 <andreacarolinaanaya2019@gmail.com>
2026-03-29 22:35:38 -0500

    Mejoras en productos, carrito funcional con cálculos, videos con autoplay forzado, hover en botones

--- src/app/views/home-component/home-component.ts
+++ src/app/views/home-component/home-component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef,  ViewChildren, AfterViewInit, QueryList, } from '@angular/core';
 import { Router } from '@angular/router';
 import { A11yModule } from "@angular/cdk/a11y";
 
@@ -17,29 +17,34 @@ export class HomeComponent implements AfterViewInit {
     this.router.navigate([path]);
   }
 
-  track = viewChild<ElementRef<HTMLDivElement>>('track');
+  @ViewChildren('track') track!: QueryList<ElementRef<HTMLDivElement>>;
+  @ViewChildren('videosa') videos!: QueryList<ElementRef<HTMLVideoElement>>;
 
   currentTranslate = 0;
 
   ngAfterViewInit() {
     this.cloneSlides();
     this.startCarousel();
+    this.videos.forEach(video => {
+      video.nativeElement.muted = true;
+      video.nativeElement.play().catch(() => {});
+    });
   }
 
+  
   cloneSlides() {
-    const trackEl = this.track()?.nativeElement;
+    const trackEl = this.track.first?.nativeElement;
     if (!trackEl) return;
 
     const slides = Array.from(trackEl.children);
     slides.forEach(slide => {
-      const clone = slide.cloneNode(true);
+      const clone = (slide as Element).cloneNode(true);
       trackEl.appendChild(clone);
     });
   }
 
   startCarousel() {
-    
-    const trackEl = this.track()?.nativeElement;
+    const trackEl = this.track.first?.nativeElement;
     if (!trackEl) return;
     const slideWidth = trackEl.firstElementChild?.clientWidth || 0;
     const gap = window.innerWidth * 0.015;
